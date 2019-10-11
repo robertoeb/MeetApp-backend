c44473a74613276b8813a8860252224407d763d5
Roberto Eustáquio <robertoeb.jr@gmail.com>
2019-10-10 22:25:54 -0300

    Create meetups migration

---
+++ src/database/migrations/20191010175002-create-meetups.js
@@ -0,0 +1,53 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('meetups', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      latitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      longitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      date_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      banner_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'files', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: queryInterface => queryInterface.dropTable('meetups'),
+};
